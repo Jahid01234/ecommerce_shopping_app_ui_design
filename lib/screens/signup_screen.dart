1b7568205692073f043317fd6b4f73ee394733ee
Jahid01234 <mdjahidhasan790523@gmail.com>
2024-07-13 18:18:09 +0600

    -some change

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,7 +1,6 @@
 import 'package:ecommerce_shopping_app_ui_design/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
-import 'navigation_screen.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -21,104 +20,94 @@ class _SignupScreenState extends State<SignupScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               //  1st image part
-              SizedBox(height: 50,),
+              const SizedBox(height: 50,),
               Image.asset("assets/images/freed.png"),
-              SizedBox(height: 50,),
+              const SizedBox(height: 50,),
               Padding(
-                padding:EdgeInsets.symmetric(horizontal: 26),
+                padding:const EdgeInsets.symmetric(horizontal: 26),
                 child: Column(
                   children: [
 
                     TextFormField(
-                      decoration: InputDecoration(
+                      decoration: const InputDecoration(
                           labelText: "Enter Name",
                           border: OutlineInputBorder(),
                           prefixIcon: Icon(Icons.person)
                       ),
                     ),
-                    SizedBox(height: 10),
+                    const SizedBox(height: 10),
 
                     // the 2nd TextField part
                     TextFormField(
-                      decoration: InputDecoration(
+                      decoration:const InputDecoration(
                           labelText: "Enter Email",
                           border: OutlineInputBorder(),
                           prefixIcon: Icon(Icons.email)
                       ),
                     ),
-                    SizedBox(height: 10),
+                    const SizedBox(height: 10),
 
                     TextFormField(
-                      decoration: InputDecoration(
+                      decoration:const  InputDecoration(
                           labelText: "Enter Number",
                           border: OutlineInputBorder(),
                           prefixIcon: Icon(Icons.numbers)
                       ),
                     ),
-                    SizedBox(height: 10),
+                    const SizedBox(height: 10),
 
                     // the 3rd TextField part
                     TextFormField(
                       obscureText: true,
-                      decoration: InputDecoration(
+                      decoration: const InputDecoration(
                         labelText: "Enter Password",
                         border: OutlineInputBorder(),
                         prefixIcon: Icon(Icons.lock),
                         suffixIcon: Icon(Icons.remove_red_eye),
                       ),
                     ),
-                    SizedBox(height: 10),
+                   const SizedBox(height: 10),
 
                     TextFormField(
                       obscureText: true,
-                      decoration: InputDecoration(
+                      decoration: const InputDecoration(
                         labelText: "Confirm Password",
                         border: OutlineInputBorder(),
                         prefixIcon: Icon(Icons.lock),
                         suffixIcon: Icon(Icons.remove_red_eye),
                       ),
                     ),
-                    SizedBox(height: 5,),
 
-                    // the 4th  Forget Password part
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: InkWell(
-                        onTap: (){},
-                        child: Text("Forget Password",
-                            style: TextStyle(fontSize: 15,color: Color(0xFFDB3022),fontWeight: FontWeight.w600)
-                        ),
-                      ),
-                    ),
-                    SizedBox(height: 50),
+                    const SizedBox(height: 50),
 
-                    // the 5th Elevated Button part
+                    // the 4th Elevated Button part
                     ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
-                      },
-                      child: Text(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:const Color(0xFFDB3022),
+                          minimumSize:const Size.fromHeight(55),
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0))),
+                      child: const Text(
                         "Create Account",
                         style: TextStyle(color: Colors.white, fontSize: 18),
                       ),
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Color(0xFFDB3022),
-                          minimumSize: Size.fromHeight(55),
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(0))),
-                    ),
-                    SizedBox(height: 10,),
+                      onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context)=> const LoginScreen()));
+                      },
 
-                    // the 6th Text  part
+                    ),
+                    const SizedBox(height: 10,),
+
+                    // the 5th Text  part
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text("Allready Have An Account?",style: TextStyle(fontSize: 15),),
-                        SizedBox(width: 3,),
+                        const Text("Already have an account?",style: TextStyle(fontSize: 15),),
+                        const SizedBox(width: 3,),
                         InkWell(
                           onTap: (){
-                            Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
+                            Navigator.push(context, MaterialPageRoute(builder: (context)=> const LoginScreen()));
                           },
-                          child: Text("Sign In",
+                          child: const Text("Sign In",
                               style: TextStyle(fontSize: 15,color: Color(0xFFDB3022),fontWeight: FontWeight.w600)
                           ),
                         )
